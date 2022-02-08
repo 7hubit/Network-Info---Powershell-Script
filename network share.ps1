3c0731aafd3bc6055104dc413f5f10e5358e0b01
7hubit <80590284+7hubit@users.noreply.github.com>
2022-02-08 09:43:35 +0200

    Add files via upload

---
+++ network share.ps1
@@ -0,0 +1,12 @@
+﻿$file = 'C:\Users\7hub\hostname.txt'
+
+if (Test-Path -Path $file) {
+    Exit
+    }
+ else {
+     Get-Date | Out-File -FilePath \\FileShare\Share\network.txt
+(Get-CimInstance -ClassName Win32_ComputerSystem).Name | Out-File -Append -FilePath \\FileShare\Share\network.txt
+Get-WmiObject Win32_NetworkAdapterConfiguration | Select Description, MACAddress, IPAddress, DefaultIPGateway, DNSServerSearchOrder | Out-File -Append -FilePath \\FileShare\Share\network.txt
+Write-Output "___________________________________________________________" | Out-File -Append -FilePath \\FileShare\Share\network.txt
+(Get-CimInstance -ClassName Win32_ComputerSystem).Name | Out-File -Append -FilePath $file
+} 
---
+++ network.ps1
@@ -0,0 +1,12 @@
+﻿$file = 'C:\Users\7hub\hostname.txt'
+
+if (Test-Path -Path $file) {
+    Exit
+    }
+ else {
+     Get-Date | Out-File -FilePath C:\Users\7hub\Desktop\network.txt
+(Get-CimInstance -ClassName Win32_ComputerSystem).Name | Out-File -Append -FilePath C:\Users\7hub\Desktop\network.txt
+Get-WmiObject Win32_NetworkAdapterConfiguration | Select Description, MACAddress, IPAddress, DefaultIPGateway, DNSServerSearchOrder | Out-File -Append -FilePath C:\Users\7hub\Desktop\network.txt
+Write-Output "___________________________________________________________" | Out-File -Append -FilePath C:\Users\7hub\Desktop\network.txt
+(Get-CimInstance -ClassName Win32_ComputerSystem).Name | Out-File -Append -FilePath $file
+} 
